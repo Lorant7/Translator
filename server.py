cfac48ba08b2feeb780cc2a6c32d0bbde4759008
Lorant7 <tomy.lorant.com>
2024-10-04 01:32:30 -0400

    initial commit

--- server.py
+++ server.py
@@ -11,17 +11,53 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/', methods=['GET'])
 def index():
-    return None
+    return render_template('index.html')
 
 
 @app.route('/speech-to-text', methods=['POST'])
 def speech_to_text_route():
-    return None
+    print("Processing Speech-to-Text")
+    audio_binary = request.data
+    text = speech_to_text(audio_binary)
+
+    response = app.respone_class(
+        response=json.dumps({'text': text}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    print(response)
+    print(response.data)
+    return response
 
 
 @app.route('/process-message', methods=['POST'])
 def process_message_route():
-    return None
+    user_message = request.json['userMessage']
+    print('user_message: ', user_message)
+
+    voice = request.json['voice']
+    print('voice: ', voice)
+
+    # Generate the response from Watson given the user's message
+    watsonx_response_text = watsonx_process_message(user_message)
+    # Clean up Watson's response cause he might fuck up
+    watsonx_response_text = os.linesep.join([s for s in watsonx_response_text.splitlines() if s])
+    # Get Watson's message in speech using the function you created in wokrer.py
+    watsonx_response_speech = text_to_speech(watsonx_response_text, voice)
+    # Conver Watson's speech into base64 string so it can be passed to the front end in JSON fromat
+    watsonx_response_speech = base64.b64encode(watsonx_response_speech).decode('utf-8')
+
+    # Create the JSON response that will be sent back to the front end
+    response = app.response_class(
+        response=json.dumps({"watsonxResponseText": watsonx_response_text, "watsonxResponseSpeech": watsonx_response_text}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    print(response)
+    return response
+
 
 
 if __name__ == "__main__":
--- worker.py
+++ worker.py
@@ -1,8 +1,81 @@
+from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes
+from ibm_watson_machine_learning.foundation_models import Model
+from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watson_machine_learning.foundation_models.utils.enums import DecodingMethods
+import requests
+
+# placeholder for Watsonx_API and Project_id incase you need to use the code outside this environment
+# API_KEY = "Your WatsonX API"
+PROJECT_ID= "skills-network"
+
+# Define the credentials 
+credentials = {
+    "url": "https://us-south.ml.cloud.ibm.com"
+    #"apikey": API_KEY
+}
+    
+# Specify model_id that will be used for inferencing
+model_id = ModelTypes.FLAN_UL2
+
+# Define the model parameters
+from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watson_machine_learning.foundation_models.utils.enums import DecodingMethods
+
+parameters = {
+    GenParams.DECODING_METHOD: DecodingMethods.GREEDY,
+    GenParams.MIN_NEW_TOKENS: 1,
+    GenParams.MAX_NEW_TOKENS: 1024
+}
+
+# Define the LLM
+model = Model(
+    model_id=model_id,
+    params=parameters,
+    credentials=credentials,
+    project_id=PROJECT_ID
+)
+
 def speech_to_text(audio_binary):
-    return None
+   base_url = "https://sn-watson-stt.labs.skills.network"
+   api_url = base_url + "/speech-to-text/api/v1/recognize"
+
+   params = {
+    'model': 'en-US_Multimedia'
+   }
+
+   body = audio_binary
+
+   response = requests.post(api_url, params=params, data=audio_binary).json()
+
+   text = 'null'
+   while bool(response.get('results')):
+    print("Specck-to-Text response: ", response)
+    text = response.get('results').pip().get('alternatives').pop().get('transcript')
+    print('recognized text: ', text)
+    return text
 
 def text_to_speech(text, voice=""):
-    return None
+    base_url = "https://sn-watson-tts.labs.skills.network"
+    api_url = base_url + "/text-to-speech/api/v1/synthesize?output=output_text.wav"
+
+    if voice != "" and voice != "default":
+        api_url += "&voice=" + voice
+
+    headers = {
+        'Accept': 'audio/wav',
+        'Content-Type': 'application/json',
+    }
+    json_data = {
+        'text': text,
+    }
+
+    response = requests.post(api_url, headers=headers, json=json_data)
+    print('Text-to-Speech response: ', response)
+    return response.content
 
 def watsonx_process_message(user_message):
-    return None
+    prompt = f"""You are an assistant helping translate sentences from English into Spanish.
+    Translate the query to Spanish: ```{user_message}```."""
+    response_text = model.generate_text(prompt=prompt)
+    print("watsonx response: ", response_text)
+    return response_text
